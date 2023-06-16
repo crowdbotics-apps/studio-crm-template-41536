d2562c4bcaf5b362d631c1c122339db1f682453e
Crowdbotics <team@crowdbotics.com>
2023-06-16 16:14:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "studio-crm-template-41536",
+  url: "https://studio-crm-template-41536.botics.co",
+  api: "https://studio-crm-template-41536.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
